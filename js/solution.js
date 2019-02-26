0c89110f1962e0fec32f12eee3786f4bf0f6d894
Sergey Beketov <beketov.serg@gmail.com>
2019-02-26 23:28:12 +0300

    добавил ограничение на загрузку файла через drop

--- js/solution.js
+++ js/solution.js
@@ -4,6 +4,7 @@ const app = document.querySelector('.app'),
       menu = app.querySelector('.menu'),
       uploadButton = menu.querySelector('.new'),
       error = app.querySelector('.error'),
+      errorMessage = error.querySelector('.error__message'),
       img = app.querySelector('img'),
       imageLoader = app.querySelector('.image-loader');
 
@@ -29,23 +30,41 @@ function setMenuState(state) {
 }
 
 
-uploadButton.addEventListener('click', loadFile);
-app.addEventListener('drop', loadImg);
+uploadButton.addEventListener('click', upload);
+app.addEventListener('drop', upload);
 app.addEventListener('dragover', event => event.preventDefault());
 
 
-function loadFile() {
+function upload() {
+  event.preventDefault();
+  const imgSrcRegExp = /^file/;
+  if(event.type !== 'drop') {
+    createInput();
+    return;
+  }
+  console.log(imgSrcRegExp.test( img.src ));
+  if(imgSrcRegExp.test( img.src )) {
+    const file = event.dataTransfer.files[0];
+    loadImg(file);
+    return;
+  }
+
+  error.setAttribute('style', 'display: inline-block');
+  errorMessage.textContent = 'Чтобы загрузить новое  изображение, пожалуйста воспользуйтесь пунктом "Загрузить новое" в меню';
+}
+
+function createInput() {
   const input = document.createElement('input');
   input.type = 'file';
   input.accept = 'image/jpeg, image/png';
   input.click();
-  input.addEventListener('change', loadImg);
+  input.addEventListener('change', () => loadImg());
 
 }
 
-function loadImg() {
-  event.preventDefault();
-  const file = event.type === 'drop' ? event.dataTransfer.files[0] : event.target.files[0];
+function loadImg(myFile = '') {
+
+  const file = myFile ? myFile : event.target.files[0];
 
   const fileTypeRegExp = /image\/jpeg|image\/png/;
   if (!fileTypeRegExp.test( file.type )) {
@@ -67,21 +86,25 @@ function loadImg() {
     })
     .then(onLoad)
     .catch(err => {
-      console.error(err);
-    });
-
-  function onLoad(response) {
-    imageLoader.setAttribute('style', 'display: none');
-    response.text().then(text => {
-      console.log(text);
-
-      if(response.ok) {
-        const data = JSON.parse(text);
-        img.src = data.url;
-        setMenuState('default');
-      }
+      console.log(err);
     });
   }
+
+
+function onLoad(response) {
+  imageLoader.setAttribute('style', 'display: none');
+  response.text().then(text => {
+    console.log(text);
+
+    if(response.ok) {
+      const data = JSON.parse(text);
+      img.src = data.url;
+      setMenuState('default');
+    } else {
+      error.setAttribute('style', 'display: inline-block');
+      errorMessage.innerHTML = text;
+    }
+  });
 
 
   // const canvas = document.createElement('canvas');
